d8cd52ed6c348b80df77265770ae51b511d16e9a
Justin Lau <zszombiesize@gmail.com>
2024-05-18 15:32:45 -0400

    express backend

--- backend/express.js
+++ backend/express.js
@@ -1,30 +1,55 @@
-const { MongoClient } = require('mongodb');
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-let client;
+let db;
 
-async function connect() {
+async function connectToDatabase() {
     const uri = "mongodb://localhost:27017";
-    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
     try {
         await client.connect();
         console.log("Connected to MongoDB!");
+        db = client.db('bubble_db');
     } catch (err) {
         console.error(err);
         process.exit(1);
     }
 }
 
-function getCollection(collectionName) {
-    return client.db("bubble_db").collection(collectionName);
-}
+app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connect();
+app.get('/', (req, res) => {
+    res.send('Welcome to BubbleMath API');
 });
 
-module.exports = { getCollection };
+app.get('/collection/:name', async (req, res) => {
+    const collectionName = req.params.name;
+    try {
+        const collection = db.collection(collectionName);
+        const documents = await collection.find({}).toArray();
+        res.json(documents);
+    } catch (err) {
+        res.status(500).send(err.toString());
+    }
+});
+
+app.post('/collection/:name', async (req, res) => {
+    const collectionName = req.params.name;
+    const document = req.body;
+    try {
+        const collection = db.collection(collectionName);
+        const result = await collection.insertOne(document);
+        res.status(201).json(result.ops[0]);
+    } catch (err) {
+        res.status(500).send(err.toString());
+    }
+});
+
+connectToDatabase().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
